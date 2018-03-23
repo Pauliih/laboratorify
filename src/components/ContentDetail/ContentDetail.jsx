9b4e43352dd06168e5422dca06711ad7b10c5f1a
Jocelyn1912 <toty_2410@hotmail.com>
2018-03-22 23:48:45 -0300

    Botones

--- src/components/ContentDetail/ContentDetail.jsx
+++ src/components/ContentDetail/ContentDetail.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 //Components
 import LabButton from "./../LabButton";
+import LabButtonGreen from "./../LabButtonGreen";
+import LabButtonPink from "./../LabButtonPink";
 import LabCard from "../LabCard";
 import './styles.css';
 
 class ContentDetail extends Component {
   render() {
     return (
-      <div className="content-detail col-sm-10">
-        <LabButton className="lab-lg">Large Button</LabButton>
-        <LabButton>Default Button</LabButton>
-        <LabButton className="lab-sm">Small Button</LabButton>
-        <LabCard />
+      <div>
+        <div className="content-detail col-sm-10">
+          <LabButton className="lab-lg">Large Button</LabButton>
+          <LabButton>Default Button</LabButton>
+          <LabButton className="lab-sm">Small Button</LabButton>
+        </div>
+        <div className="content-detail col-sm-10">
+          <LabButtonGreen className="lab-lg">Large Button</LabButtonGreen>
+          <LabButtonGreen>Default Button</LabButtonGreen>
+          <LabButtonGreen className="lab-sm">Small Button</LabButtonGreen>
+        </div>
+        <div className="content-detail col-sm-10">
+          <LabButtonPink className="lab-lg">Large Button</LabButtonPink>
+          <LabButtonPink>Default Button</LabButtonPink>
+          <LabButtonPink className="lab-sm">Small Button</LabButtonPink>
+          <LabCard />
+        </div>
       </div>
     );
   }
---
+++ src/components/LabButtonGreen.jsx
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'; // ES6
+
+class LabButtonGreen extends Component {
+  render() {
+    var laboratorify = {};
+
+    switch (this.props.className) {
+      case 'lab-lg':
+        laboratorify = {
+          cursor: 'pointer',
+          backgroundColor: '#56F89A',
+          borderColor: '#56F89A',
+          fontFamily: 'Open Sans',
+          color: '#000',
+          display: 'inline-block',
+          weight: 800,
+          textAlign: 'center',
+          verticalAlign: 'middle',
+          padding: '0.5rem 1rem',
+          fontSize:'1.25rem',
+          lineHeight: 1.5,
+          borderRadius: '0.3rem'
+        }
+        break;
+      case 'lab-sm':
+        laboratorify = {
+          cursor: 'pointer',
+          backgroundColor: '#56F89A',
+          borderColor: '#56F89A',
+          fontFamily: 'Open Sans',
+          color: '#000',
+          display: 'inline-block',
+          weight: 800,
+          textAlign: 'center',
+          verticalAlign: 'middle',
+          padding: '0.25rem 0.5rem',
+          fontSize: '0.875rem',
+          lineHeight: 1.5,
+          borderRadius: '0.2rem'
+        }
+        break;
+      default:
+        laboratorify = {
+          cursor: 'pointer',
+          backgroundColor: '#56F89A',
+          borderColor: '#56F89A',
+          fontFamily: 'Open Sans',
+          color: '#000',
+          display: 'inline-block',
+          weight: 800,
+          textAlign: 'center',
+          verticalAlign: 'middle',
+          padding: '0.375rem 0.75rem',
+          fontSize: '1rem',
+          lineHeight: 1.5,
+          borderRadius: '0.25rem'
+        }
+        break;
+    }
+
+    // Proptypes
+    LabButtonGreen.propTypes = {
+      laboratorify: PropTypes.shape({
+        cursor: PropTypes.string,
+        backgroundColor: PropTypes.string,
+        borderColor: PropTypes.string,
+        fontFamily: PropTypes.string,
+        color: PropTypes.string,
+        display: PropTypes.string,
+        weight: PropTypes.number,
+        textAlign: PropTypes.string,
+        verticalAlign: PropTypes.string,
+        padding: PropTypes.string,
+        fontSize: PropTypes.string,
+        lineHeight: PropTypes.number,
+        borderRadius: PropTypes.string
+      })
+    };
+
+    return (
+      <button style={laboratorify}>
+        {this.props.children}
+      </button>
+    );
+  }
+}
+
+export default LabButtonGreen;
